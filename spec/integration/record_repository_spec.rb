e4b5c640efac43523a7543e21112b615f779b1f8
Josh Williams <josh@spacesnottabs.com>
2022-11-20 17:01:21 +0000

    Added integration test for record repository

---
+++ spec/integration/record_repository_spec.rb
@@ -0,0 +1,46 @@
+require 'elasticsearch'
+require 'register_sources_sk/repositories/record_repository'
+require 'register_sources_sk/services/es_index_creator'
+require 'register_sources_sk/structs/record'
+
+RSpec.describe RegisterSourcesSk::Repositories::RecordRepository do
+  subject { described_class.new(client: es_client, index: index) }
+
+  let(:index) { SecureRandom.uuid }
+  let(:es_client) do
+    Elasticsearch::Client.new(
+      host: "http://elastic:#{ENV['ELASTICSEARCH_PASSWORD']}@#{ENV['ELASTICSEARCH_HOST']}:#{ENV['ELASTICSEARCH_PORT']}",
+      transport_options: { ssl: { verify: false } },
+      log: false
+    )
+  end
+
+  let(:record) do
+    RegisterSourcesSk::Record[
+      JSON.parse(File.read('spec/fixtures/sk_company_datum.json'))
+    ]
+  end
+
+  before do
+    index_creator = RegisterSourcesSk::Services::EsIndexCreator.new(
+      es_index: index,
+      client: es_client
+    )
+    index_creator.create_index
+  end
+
+  describe '#store' do
+    it 'stores' do
+      records = [record]
+
+      subject.store(records)
+
+      sleep 1 # eventually consistent, give time
+
+      expect(subject.get(record.etag)).to eq record
+
+      # When records do not exist
+      expect(subject.get("missing")).to be_nil
+    end
+  end
+end
