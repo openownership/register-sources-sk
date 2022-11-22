230b22980a680328559d3c45f3124c48691775bb
Josh Williams <josh@spacesnottabs.com>
2022-11-22 00:13:25 +0000

    Removed invalid struct attribute

--- lib/register_sources_sk/structs/record.rb
+++ lib/register_sources_sk/structs/record.rb
@@ -9,12 +9,10 @@ module RegisterSourcesSk
   class Record < Dry::Struct
     transform_keys(&:to_sym)
 
-    # attribute? :"@odata.context", Types::String.optional
     attribute? :Id, Types::Nominal::Integer.optional
     attribute? :CisloVlozky, Types::Nominal::Integer.optional # Insert number
     attribute? :PartneriVerejnehoSektora, Types.Array(PartneriVerejnehoSektora) # Public Sector Partners
     attribute? :KonecniUzivateliaVyhod, Types.Array(KonecniUzivateliaVyhod) # End Users Benefits
-    # PartneriVerejnehoSektora@odata.nextLink
 
     def etag
       @etag ||= XXhash.xxh64(to_h.to_json).to_s
