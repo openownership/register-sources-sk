8feff9af7500a23720289a9035e6c8a2d6b29d39
Josh Williams <josh@spacesnottabs.com>
2023-09-14 20:50:51 +0000

    Fixed unit tests for record repository

--- spec/integration/record_repository_spec.rb
+++ spec/integration/record_repository_spec.rb
@@ -5,8 +5,6 @@ require 'register_sources_sk/repositories/record_repository'
 require 'register_sources_sk/services/es_index_creator'
 require 'register_sources_sk/structs/record'
 
-BodsIdentifier = Struct.new(:id, :schemeName)
-
 RSpec.describe RegisterSourcesSk::Repositories::RecordRepository do
   subject { described_class.new(client: es_client, index:) }
 
@@ -39,36 +37,6 @@ RSpec.describe RegisterSourcesSk::Repositories::RecordRepository do
 
       # When records do not exist
       expect(subject.get('missing')).to be_nil
-    end
-  end
-
-  describe '#get_by_bods_identifiers' do
-    it 'retrieves' do
-      records = [record]
-
-      subject.store(records)
-
-      sleep 1 # eventually consistent, give time
-
-      id_identifiers = [
-        BodsIdentifier.new('3', 'SK Register Partnerov Verejného Sektora')
-      ]
-      expect(subject.get_by_bods_identifiers(id_identifiers)).to eq [record]
-
-      ico_identifiers = [
-        BodsIdentifier.new('1234567', 'Ministry of Justice Business Register')
-      ]
-      expect(subject.get_by_bods_identifiers(ico_identifiers)).to eq [record]
-
-      missing_id_identifiers = [
-        BodsIdentifier.new('10', 'SK Register Partnerov Verejného Sektora')
-      ]
-      expect(subject.get_by_bods_identifiers(missing_id_identifiers)).to eq []
-
-      missing_ico_identifiers = [
-        BodsIdentifier.new('1234568', 'Ministry of Justice Business Register')
-      ]
-      expect(subject.get_by_bods_identifiers(missing_ico_identifiers)).to eq []
     end
   end
 end
